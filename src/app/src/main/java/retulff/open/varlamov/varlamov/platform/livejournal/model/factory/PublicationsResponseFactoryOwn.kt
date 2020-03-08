ecf4d54c70cd398807db560462415240e101803c
0gdump <0gdump.kv@gmail.com>
2020-03-08 23:55:18 +1000

    Removed unused old factories

--- src/app/src/main/java/retulff/open/varlamov/varlamov/platform/livejournal/model/factory/PublicationsResponseFactoryOwn.kt
+++
@@ -1,9 +0,0 @@
-package retulff.open.varlamov.varlamov.platform.livejournal.model.factory
-
-import android.util.Base64
-import org.apache.commons.lang3.StringUtils.isNumeric
-import org.joda.time.DateTimeZone
-import retulff.open.varlamov.util.TimeUtils
-import retulff.open.varlamov.varlamov.platform.livejournal.model.Publication
-
-typealias Key = Pair<String, Int>
