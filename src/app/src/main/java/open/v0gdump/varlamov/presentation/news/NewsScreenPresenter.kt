adab2e5df903b5d0532030b6f7b22af9de4696e4
Klachkov Valery <0gdump.kv@gmail.com>
2020-04-30 02:07:42 +1000

    [ui/news] Implement news fragment

--- src/app/src/main/java/open/v0gdump/varlamov/presentation/news/NewsScreenPresenter.kt
+++ src/app/src/main/java/open/v0gdump/varlamov/presentation/news/NewsScreenPresenter.kt
@@ -1,7 +1,79 @@
 package open.v0gdump.varlamov.presentation.news
 
+import kotlinx.coroutines.channels.consumeEach
+import kotlinx.coroutines.launch
 import moxy.InjectViewState
-import moxy.MvpPresenter
+import okhttp3.ResponseBody
+import open.v0gdump.varlamov.model.Blog
+import open.v0gdump.varlamov.model.platform.livejournal.model.Publication
+import open.v0gdump.varlamov.model.platform.livejournal.model.PublicationsFactory
+import open.v0gdump.varlamov.presentation.global.MvpPresenterX
+import open.v0gdump.varlamov.presentation.global.Paginator
+import org.joda.time.DateTime
+import org.joda.time.DateTimeZone
+import retrofit2.Call
+import retrofit2.Response
 
 @InjectViewState
-class NewsScreenPresenter : MvpPresenter<NewsScreenView>()
+class NewsScreenPresenter : MvpPresenterX<NewsScreenView>() {
+
+    private val paginator = Paginator.Store<Publication>()
+
+    init {
+        paginator.render = { viewState.renderPaginatorState(it) }
+        launch {
+            paginator.sideEffects.consumeEach { effect ->
+                when (effect) {
+                    is Paginator.SideEffect.LoadPage -> {
+                        loadNewPage(effect.currentPage, effect.currentPageLastItem as Publication?)
+                    }
+                    is Paginator.SideEffect.ErrorEvent -> {
+                        viewState.showMessage(effect.error.message.orEmpty())
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onFirstViewAttach() {
+        super.onFirstViewAttach()
+        refresh()
+    }
+
+    private fun loadNewPage(page: Int, lastItem: Publication?) {
+
+        val beforeDate =
+            lastItem?.time?.withZone(DateTimeZone.UTC) ?: DateTime.now().withZone(DateTimeZone.UTC)
+
+        Blog.getPublicationsByTag(
+            "p-news",
+            20,
+            beforeDate,
+            object : retrofit2.Callback<ResponseBody> {
+                override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                    paginator.proceed(Paginator.Action.PageError(t))
+                }
+
+                override fun onResponse(
+                    call: Call<ResponseBody>,
+                    response: Response<ResponseBody>
+                ) {
+                    if (!response.isSuccessful || response.body() == null) {
+                        paginator.proceed(Paginator.Action.PageError(Throwable()))
+                        return
+                    }
+
+                    val responseBody = response.body()!!.string()
+                    val publications = PublicationsFactory.convert(responseBody)
+
+                    paginator.proceed(Paginator.Action.NewPage(page + 1, publications))
+                }
+            }
+        )
+    }
+
+    fun refresh() = paginator.proceed(Paginator.Action.Refresh)
+    fun loadMore() = paginator.proceed(Paginator.Action.LoadMore)
+
+    fun onPublicationClicked(publication: Publication) {}
+}
--- src/app/src/main/java/open/v0gdump/varlamov/ui/news/NewsScreen.kt
+++ src/app/src/main/java/open/v0gdump/varlamov/ui/news/NewsScreen.kt
@@ -1,12 +1,53 @@
 package open.v0gdump.varlamov.ui.news
 
+import android.os.Bundle
+import es.dmoral.toasty.Toasty
+import kotlinx.android.synthetic.main.fragment_by_tag.*
+import moxy.ktx.moxyPresenter
 import open.v0gdump.varlamov.R
+import open.v0gdump.varlamov.model.platform.livejournal.model.Publication
+import open.v0gdump.varlamov.presentation.global.Paginator
+import open.v0gdump.varlamov.presentation.news.NewsScreenPresenter
 import open.v0gdump.varlamov.presentation.news.NewsScreenView
 import open.v0gdump.varlamov.ui.global.MvpFragmentX
+import open.v0gdump.varlamov.ui.global.PublicationsAdapterDelegate
+import open.v0gdump.varlamov.ui.global.view.PaginalAdapter
 
 class NewsScreen : MvpFragmentX(R.layout.fragment_by_tag), NewsScreenView {
 
-    override fun setupLayout() {
+    private val presenter by moxyPresenter { NewsScreenPresenter() }
 
+    override fun setupLayout() {}
+
+    private val adapter by lazy {
+        PaginalAdapter(
+            { presenter.loadMore() },
+            { o, n ->
+                if (o is Publication && n is Publication)
+                // TODO Требуется быстро сравнивать 2 публикации, иначе возникают странные дергания
+                    false
+                else
+                    false
+            },
+            PublicationsAdapterDelegate {
+                presenter.onPublicationClicked(it)
+            }
+        )
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        paginalRenderView.init(
+            { presenter.refresh() },
+            adapter
+        )
+    }
+
+    override fun renderPaginatorState(state: Paginator.State) {
+        paginalRenderView.render(state)
+    }
+
+    override fun showMessage(message: String) {
+        Toasty.info(activity, message).show()
     }
 }
