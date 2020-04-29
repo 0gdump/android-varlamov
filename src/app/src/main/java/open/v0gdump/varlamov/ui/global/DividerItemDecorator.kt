6fe08b14ef317a2763e9eb87f019ec9257ee9db0
Klachkov Valery <0gdump.kv@gmail.com>
2020-04-30 01:54:50 +1000

    [ui/global] Rewrite DividerItemDecorator

--- src/app/src/main/java/open/v0gdump/varlamov/ui/global/DividerItemDecorator.kt
+++ src/app/src/main/java/open/v0gdump/varlamov/ui/global/DividerItemDecorator.kt
@@ -1,26 +1,46 @@
 package open.v0gdump.varlamov.ui.global
 
+import android.content.Context
 import android.graphics.Canvas
 import android.graphics.drawable.Drawable
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ItemDecoration
 
-class DividerItemDecorator(private val mDivider: Drawable) : ItemDecoration() {
-    override fun onDraw(canvas: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+class DividerItemDecorator : ItemDecoration {
 
-        val dividerLeft = parent.paddingLeft
-        val dividerRight = parent.width - parent.paddingRight
-        val childCount = parent.childCount
+    private val attrs = intArrayOf(android.R.attr.listDivider)
+    private var divider: Drawable?
 
-        for (i in 0 until childCount) {
-            val child = parent.getChildAt(i)
-            val params = child.layoutParams as RecyclerView.LayoutParams
-            val dividerTop: Int = child.bottom + params.bottomMargin
-            val dividerBottom = dividerTop + mDivider.intrinsicHeight
+    constructor(context: Context) {
+        val styledAttributes = context.obtainStyledAttributes(attrs)
 
-            mDivider.setBounds(dividerLeft, dividerTop, dividerRight, dividerBottom)
-            mDivider.draw(canvas)
-        }
+        divider = styledAttributes.getDrawable(0)
+
+        styledAttributes.recycle()
     }
 
+    constructor(context: Context, resId: Int) {
+        divider = ContextCompat.getDrawable(context, resId)
+    }
+
+    override fun onDraw(canvas: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+
+        if (divider == null) return
+
+        val left = parent.paddingLeft
+        val right = parent.width - parent.paddingRight
+
+        (0 until parent.childCount - 1).forEach { i ->
+
+            val child = parent.getChildAt(i)
+            val params = child.layoutParams as RecyclerView.LayoutParams
+
+            val top = child.bottom + params.bottomMargin
+            val bottom = top + divider!!.intrinsicHeight
+
+            divider!!.setBounds(left, top, right, bottom)
+            divider!!.draw(canvas)
+        }
+    }
 }
