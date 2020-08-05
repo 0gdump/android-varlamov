0724338f665a17cfe1a2877b97adb0fe736408ae
Klachkov Valery <0gdump.kv@gmail.com>
2020-08-05 11:49:09 +1000

    [ui] Keep alive fragments while navigation

--- src/app/src/main/java/open/v0gdump/varlamov/ui/main/MainScreen.kt
+++ src/app/src/main/java/open/v0gdump/varlamov/ui/main/MainScreen.kt
@@ -1,20 +1,22 @@
 package open.v0gdump.varlamov.ui.main
 
 import androidx.navigation.Navigation
+import androidx.navigation.plusAssign
 import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.fragment_main.view.*
 import open.v0gdump.varlamov.R
 import open.v0gdump.varlamov.ui.global.MvpFragmentX
+import open.v0gdump.varlamov.util.navigation.KeepStateNavigator
 
 class MainScreen : MvpFragmentX(R.layout.fragment_main) {
 
-    private val navController by lazy {
-        Navigation.findNavController(
-            layout.findViewById(R.id.container)
-        )
-    }
-
     override fun setupLayoutOnCreate() {
-        layout.bottom_navigation.setupWithNavController(navController)
+        super.setupLayoutOnCreate()
+        Navigation.findNavController(layout.findViewById(R.id.container)).let {
+            val navigator = KeepStateNavigator(activity, childFragmentManager, R.id.container)
+            it.navigatorProvider += navigator
+            it.setGraph(R.navigation.main)
+            layout.bottom_navigation.setupWithNavController(it)
+        }
     }
 }
