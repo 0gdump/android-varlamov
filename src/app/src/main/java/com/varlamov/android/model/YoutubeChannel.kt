863b8dfe1ff59949a9f5ec424b5068561e0c8536
Klachkov Valery <0gdump.kv@gmail.com>
2020-08-07 00:03:18 +1000

    [model/youtube] Save private key inside apikey.properties

--- src/app/src/main/java/com/varlamov/android/model/YoutubeChannel.kt
+++ src/app/src/main/java/com/varlamov/android/model/YoutubeChannel.kt
@@ -1,20 +1,18 @@
 package com.varlamov.android.model
 
+import com.varlamov.android.BuildConfig
 import com.varlamov.android.model.platform.youtube.model.VideosRequestResponse
 import com.varlamov.android.model.platform.youtube.service.YoutubeService
 import retrofit2.Callback
 
 object YoutubeChannel {
 
-    // FIXME Ключи были деактивированы. Брать их из env
-    private const val AUTH_KEY = "-"
-    private const val CHANNEL_ID = "UC101o-vQ2iOj9vr00JUlyKw"
-
     private val youtubeService = YoutubeService.create()
 
     fun getLatestVideos(count: Int, callback: Callback<VideosRequestResponse>) =
         youtubeService.getLatestVideo(
-            AUTH_KEY,
-            CHANNEL_ID, count
+            BuildConfig.YOUTUBE_KEY,
+            "UC101o-vQ2iOj9vr00JUlyKw",
+            count
         ).enqueue(callback)
 }
