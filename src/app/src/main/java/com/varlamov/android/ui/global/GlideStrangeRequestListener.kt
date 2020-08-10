5e517dc2db5a97920f4bd5a889b50bad16666846
Klachkov Valery <0gdump.kv@gmail.com>
2020-08-10 18:04:19 +1000

    [ui] Implement tags

---
+++ src/app/src/main/java/com/varlamov/android/ui/global/GlideStrangeRequestListener.kt
@@ -0,0 +1,30 @@
+package com.varlamov.android.ui.global
+
+import android.graphics.drawable.Drawable
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+
+class GlideStrangeRequestListener(
+    private val onLoadFailedCallback: () -> Unit
+) : RequestListener<Drawable> {
+
+    override fun onLoadFailed(
+        e: GlideException?,
+        model: Any?,
+        target: Target<Drawable>?,
+        isFirstResource: Boolean
+    ): Boolean {
+        onLoadFailedCallback()
+        return false
+    }
+
+    override fun onResourceReady(
+        resource: Drawable?,
+        model: Any?,
+        target: Target<Drawable>?,
+        dataSource: DataSource?,
+        isFirstResource: Boolean
+    ) = false
+}
--- src/app/src/main/java/com/varlamov/android/ui/global/PublicationsAdapterDelegate.kt
+++ src/app/src/main/java/com/varlamov/android/ui/global/PublicationsAdapterDelegate.kt
@@ -1,29 +1,38 @@
 package com.varlamov.android.ui.global
 
-import android.graphics.drawable.Drawable
+import android.R.attr
+import android.content.Context
+import android.content.res.ColorStateList
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.view.ContextThemeWrapper
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.DataSource
-import com.bumptech.glide.load.engine.GlideException
-import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.RequestOptions
-import com.bumptech.glide.request.target.Target
+import com.google.android.material.chip.Chip
 import com.hannesdorfmann.adapterdelegates4.AdapterDelegate
+import com.varlamov.android.App
 import com.varlamov.android.R
 import com.varlamov.android.model.platform.livejournal.model.Publication
 import com.varlamov.android.util.android.inflate
 import com.varlamov.android.util.android.visible
-import com.varlamov.android.util.kotlin.getWords
 import com.varlamov.android.util.toString
 import kotlinx.android.synthetic.main.item_text_and_image_post.view.*
-import org.jsoup.Jsoup
 import java.util.*
+
 
 class PublicationsAdapterDelegate(
     private val clickListener: (Publication) -> Unit
 ) : AdapterDelegate<MutableList<Any>>() {
+
+    companion object {
+        private val glideRequestOptions = RequestOptions()
+            .centerCrop()
+            .placeholder(R.drawable.ic_preview_wait)
+            .error(R.drawable.ic_preview_error)
+    }
 
     override fun isForViewType(items: MutableList<Any>, position: Int) =
         items[position] is Publication
@@ -43,71 +52,68 @@ class PublicationsAdapterDelegate(
         private lateinit var publication: Publication
 
         fun bind(publication: Publication) {
-
             this.publication = publication
 
             bindContent()
             loadPreview()
-
-            itemView.setOnClickListener { clickListener.invoke(publication) }
+            setListeners()
         }
 
+        // TODO(PERF) Check JodaTime perfomance
+        // TODO(CODE) Show text preview
         private fun bindContent() {
+            itemView.date.text = publication.time.toString(TimeZone.getDefault())
+            itemView.title.text = publication.title
 
-            // TODO Check performance
-            val time = publication.time.toString(TimeZone.getDefault())
-            val title = publication.title
-            val itemParsedContent = Jsoup.parse(publication.content)
-            val text = "${itemParsedContent.text().getWords(20)}..."
+            if (publication.tags.contains(App.res.getString(R.string.news_tag))) {
+                showNewsChip()
+            } else {
+                createTagsChips()
+            }
+        }
 
-            itemView.date.text = time
-            itemView.title.text = title
-            //itemView.publication_text.text = text
+        private fun showNewsChip() {
+            itemView.tagsContainer.visible(false)
+            itemView.newsLabel.visible(true)
+        }
+
+        private fun createTagsChips() {
+            itemView.tagsContainer.visible(true)
+            itemView.newsLabel.visible(false)
+
+            itemView.tagsContainer.tags = publication.tags
         }
 
         private fun loadPreview() {
-
-            val imgSourceRegex = Regex("src=\"([^\"]+)\"")
-            val imgSourceMatch = imgSourceRegex.find(publication.content)
-
-            if (imgSourceMatch == null || imgSourceMatch.groupValues.size < 2) {
+            val previewUrl = extractPreviewUrl()
+            if (previewUrl == null) {
                 itemView.preview.visible(false)
                 return
-            } else {
-                itemView.preview.visible(true)
             }
 
-            val imgSource = imgSourceMatch.groupValues[1]
-            val glideOptions = RequestOptions()
-                .centerCrop()
-                .placeholder(R.drawable.ic_preview_wait)
-                .error(R.drawable.ic_preview_error)
+            itemView.preview.visible(true)
 
             Glide
                 .with(itemView)
-                .load(imgSource)
-                .apply(glideOptions)
-                .listener(object : RequestListener<Drawable> {
-
-                    override fun onLoadFailed(
-                        e: GlideException?,
-                        model: Any?,
-                        target: Target<Drawable>?,
-                        isFirstResource: Boolean
-                    ): Boolean {
-                        itemView.preview.visible(false)
-                        return false
-                    }
-
-                    override fun onResourceReady(
-                        resource: Drawable?,
-                        model: Any?,
-                        target: Target<Drawable>?,
-                        dataSource: DataSource?,
-                        isFirstResource: Boolean
-                    ) = false
-                })
+                .load(previewUrl)
+                .apply(glideRequestOptions)
+                .listener(GlideStrangeRequestListener { itemView.preview.visible(false) })
                 .into(itemView.preview)
+        }
+
+        private fun extractPreviewUrl(): String? {
+            val imgSourceRegex = Regex("src=\"([^\"]+)\"")
+            val imgSourceMatch = imgSourceRegex.find(publication.content)
+
+            return if (imgSourceMatch == null || imgSourceMatch.groupValues.size < 2) {
+                null
+            } else {
+                imgSourceMatch.groupValues[1]
+            }
+        }
+
+        private fun setListeners() {
+            itemView.setOnClickListener { clickListener.invoke(publication) }
         }
     }
 }
